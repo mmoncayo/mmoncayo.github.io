06154ad2e1cfe54b331b4e7c8effd6eaa9117960
Matt Moncayo <matthew.moncayo@gmail.com>
2020-09-23 14:13:21 -0700

    production build ready

---
+++ build/precache-manifest.778917556a563007a6ecf51a349b905a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "786660e518feb6ea6b497c1694857c6d",
+    "url": "mmoncayo.github.io/index.html"
+  },
+  {
+    "revision": "bcef25a62d0166e6ce0f",
+    "url": "mmoncayo.github.io/static/css/main.89c3ac69.chunk.css"
+  },
+  {
+    "revision": "b3dd258e6d859c9151ab",
+    "url": "mmoncayo.github.io/static/js/2.a8ad0f14.chunk.js"
+  },
+  {
+    "revision": "bcef25a62d0166e6ce0f",
+    "url": "mmoncayo.github.io/static/js/main.ca620f62.chunk.js"
+  },
+  {
+    "revision": "818b5255b7b5ad209fea",
+    "url": "mmoncayo.github.io/static/js/runtime~main.3f1ae9d9.js"
+  },
+  {
+    "revision": "436c6306ead97402a46301bc517aa42d",
+    "url": "mmoncayo.github.io/static/media/achilles-heel.436c6306.png"
+  }
+]);
